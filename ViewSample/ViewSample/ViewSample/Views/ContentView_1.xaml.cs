922c507b46c8b295c7d2416efc3b96a46d48c889
PV Truong <TruongPV@PPM09-PC.hn-binhanh.local>
2017-10-04 10:01:54 +0700

    ContentView Example

---
+++ ViewSample/ViewSample/ViewSample/Views/ContentView_1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ViewSample.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContentView_1 : ContentView
+    {
+        public ContentView_1()
+        {
+            InitializeComponent();
+        }
+    }
+}
